11d48221e54c2b8a55d6a7848a85bbb17a7953c6
Don Neill <neilljd@gmail.com>
2018-09-18 16:09:58 -0700

    use git describe to generate version string

--- drpifu/Version.py
+++ drpifu/Version.py
@@ -4,13 +4,13 @@ import datetime
 
 
 def ifu_drp_version():
-    # try:
-    #    cwd = os.getcwd()
-    #    home = os.environ["HOME"]
-    #    os.chdir(home+"/sedmpy/drpifu")
-    #    ver = subprocess.check_output(["git", "describe", "--always"]).strip()
-    #    os.chdir(cwd)
-    #except:
-    ver = "%s" % datetime.datetime.now()
+    try:
+        cwd = os.getcwd()
+        home = os.environ["HOME"]
+        os.chdir(home+"/sedmpy/drpifu")
+        ver = subprocess.check_output(["git", "describe", "--always"]).strip()
+        os.chdir(cwd)
+    except:
+        ver = "%s" % datetime.datetime.now()
 
     return str(ver)
