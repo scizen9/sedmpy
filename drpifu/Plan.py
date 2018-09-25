cf1e19eb98397f68c39af77dc96f232122f6987e
Don Neill <neilljd@gmail.com>
2018-09-25 14:45:12 -0700

    rename marshal submodule to growth

--- drpifu/Plan.py
+++ drpifu/Plan.py
@@ -134,11 +134,11 @@ def identify_observations(headers):
 make_preamble = """
 PY = ~/spy
 PYC = ~/sedmpy/drpifu
-PYM = ~/sedmpy/marshal
+PYG = ~/sedmpy/growth
 IMCOMBINE = $(PY) $(PYC)/Imcombine.py
 REPORT = $(PY) $(PYC)/DrpReport.py
 CLASS = $(PY) $(PYC)/Classify.py
-ZTFUPLOAD = $(PY) $(PYM)/growth.py
+ZTFUPLOAD = $(PY) $(PYG)/growth.py
 
 BSUB = $(PY) $(PYC)/Debias.py
 CRRSUB =  $(PY) $(PYC)/CosmicX.py
