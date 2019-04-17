0563f78833a9323dac2eb2f50ac499d9ceb77feb
Don Neill <neilljd@gmail.com>
2019-04-17 14:49:40 -0700

    change open mode for text file

--- drpifu/EffTrend.py
+++ drpifu/EffTrend.py
@@ -116,7 +116,7 @@ pl.ylim(-1, 15)
 ofil = os.path.join(sdir, 'SEDM_eff_trend_pysedm.pdf')
 tfil = os.path.join(sdir, 'SEDM_eff_trend_pysedm.txt')
 pl.savefig(ofil)
-with open(tfil, 'wb') as dfil:
+with open(tfil, 'w') as dfil:
     dat_writer = csv.writer(dfil, delimiter=" ", quoting=csv.QUOTE_MINIMAL)
     dat_writer.writerows(zip(jd, ef1, ef2, ef3, ef4, ef5, efs))
 
