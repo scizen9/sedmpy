e33e3ecae3dd276053c69f1a984793cf6e096030
Don Neill <neilljd@gmail.com>
2019-11-18 15:50:31 -0800

    update HdrFix.py to be consistent

--- drpifu/HdrFix.py
+++ drpifu/HdrFix.py
@@ -1,29 +1,97 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-import os
 import glob
 import argparse
 import logging
 import astropy.io.fits as pf
 
+header_types = {"TELESCOP": str, "LST": str,
+                "MJD_OBS": float, "JD": float,
+                "APPEQX": float, "EQUINOX": int, "TEL_HA": str,
+                "RA": str, "TEL_RA": str, "DEC": str, "TEL_DEC": str,
+                "TEL_AZ": float, "TEL_EL": float, "AIRMASS": float,
+                "TEL_PA": float, "RA_RATE": float, "DEC_RATE": float,
+                "RA_OFF": float, "DEC_OFF": float,
+                "TELHASP": float, "TELDECSP": float,
+                "HA_REFR": float, "DEC_REFR": float, "FOCPOS": float,
+                "IFUFOCUS": float, "IFUFOC2": float, "DOMEST": str,
+                "DOMEMO": str, "DOME_GAP": float, "DOMEAZ": float,
+                "WSCRMO": str, "TELCONT": str,
+                "LAMPSTAT": str, "LAMPCUR": float,
+                "HG_LAMP": str, "XE_LAMP": str, "CD_LAMP": str,
+                "TELPOWST": str, "OILSTAT": str, "WEASTAT": str, "SUNSTAT": str,
+                "REMOTST": str, "TELRDST": str, "HAAX_ST": str, "FOCSTAT": str,
+                "DEC_AX": str, "OBJECT": str, "OBJTYPE": str, "IMGTYPE": str,
+                "OBJNAME": str, "OBJEQX": str, "OBJRA": str, "OBJDEC": str,
+                "ORA_RAT": float, "ODEC_RAT": float,
+                "SUNRISE": str, "SUNSET": str,
+                "TEL_MO": str, "WSCR_EL": float, "SOL_RA": str, "SOL_DEC": str,
+                "WIND_DIR": str, "WSP_CUR": float, "WSP_AVG": float,
+                "OUT_AIR": float, "OUT_HUM": float, "OUT_DEW": float,
+                "IN_AIR": float, "IN_HUM": float, "IN_DEW": float,
+                "MIR_TEMP": float, "TOP_AIR": float, "PRI_TEMP": float,
+                "SEC_TEMP": float, "FLO_TEMP": float, "BOT_TEMP": float,
+                "MID_TEMP": float, "TOP_TEMP": float, "WETNESS": float,
+                "FILTER": str, "ABPAIR": bool, "IMGSET": str, "NAME": str,
+                "UTC": str, "OBSDATE": str, "OBSTIME": str, "P60PRID": str,
+                "P60PRNM": str, "P60PRPI": str, "EMAIL": str, "INSTRUME": str,
+                "REQ_ID": int, "OBJ_ID": int, "CRVAL1": float, "CRVAL2": float,
+                "BARPRESS": float, "SECPRESS": float,
+                "END_SHUT": str, "ENDAIR": float, "ENDDOME": str,
+                "END_RA": str, "END_DEC": str, "END_PA": float,
+                "ENDBARPR": float, "ENDSECPR": float, "ELAPTIME": float}
+
 
 def sedm_fix_header(fname):
     """Make sure required keywords are present and correct"""
     ff = pf.open(fname, 'update')
+    # lamp status
+    lamps_dic = {'LAMPSTAT': 'off', 'HG_LAMP': 'off',
+                 'CD_LAMP': 'off', 'XE_LAMP': 'off'}
     # image type
     if 'OBJECT' in ff[0].header:
         obj = ff[0].header['OBJECT']
         if 'STD-' in obj:
             ff[0].header['IMGTYPE'] = 'Standard'
+            ff[0].header['ABPAIR'] = False
         elif 'Calib' in obj:
             if 'dome' in obj:
                 ff[0].header['IMGTYPE'] = 'dome'
+                lamps_dic['LAMPSTAT'] = 'on'
             elif 'bias' in obj:
                 ff[0].header['IMGTYPE'] = 'bias'
             else:
                 ff[0].header['IMGTYPE'] = 'lamp'
+                if 'Hg' in obj:
+                    lamps_dic['HG_LAMP'] = 'on'
+                elif 'Cd' in obj:
+                    lamps_dic['CD-LAMP'] = 'on'
+                elif 'Xe' in obj:
+                    lamps_dic['XE_LAMP'] = 'on'
+                else:
+                    logging.warning("Unknown lamp type")
+            ff[0].header['ABPAIR'] = False
         else:
             ff[0].header['IMGTYPE'] = 'Science'
+            # ABPAIR status (assume true unless otherwise)
+            if 'ABPAIR' not in ff[0].header:
+                ff[0].header['ABPAIR'] = True
+            else:
+                if type(ff[0].header['ABPAIR']) != bool:
+                    if type(ff[0].header['ABPAIR']) == str:
+                        ff[0].header['ABPAIR'] = ('T' in ff[0].header['ABPAIR'])
+                    else:
+                        ff[0].header['ABPAIR'] = True
+    # Set lamps
+    for k, v in lamps_dic.items():
+        # Check current value
+        if k in ff[0].header:
+            # If null, replace with dic value
+            if len(ff[0].header[k]) <= 0:
+                ff[0].header[k] = v
+        # If not in header, use dic value
+        else:
+            ff[0].header[k] = v
     # RA rate
     if 'RA_RATE' not in ff[0].header:
         if 'RARATE' in ff[0].header:
@@ -46,7 +114,7 @@ def sedm_fix_header(fname):
             rel_hum = ff[0].header['Inside_Rel_Hum']
             ff[0].header['IN_HUM'] = rel_hum
         else:
-            ff[0].header['IN_HUM'] = -1.
+            ff[0].header['IN_HUM'] = -999.
             logging.warning("No relative humidity")
     # Temperature
     if 'IN_AIR' not in ff[0].header:
@@ -54,7 +122,7 @@ def sedm_fix_header(fname):
             in_temp = ff[0].header['Inside_Air_Temp']
             ff[0].header['IN_AIR'] = in_temp
         else:
-            ff[0].header['IN_AIR'] = -1.
+            ff[0].header['IN_AIR'] = -999.
             logging.warning("No inside temperature")
     # Parallactic Angle
     if 'TEL_PA' not in ff[0].header:
@@ -63,7 +131,7 @@ def sedm_fix_header(fname):
             ff[0].header['TEL_PA'] = tel_pa
         else:
             logging.warning("No telescope PA")
-            ff[0].header['TEL_PA'] = -1.
+            ff[0].header['TEL_PA'] = -999.
     # MJD Obs
     if 'MJD_OBS' not in ff[0].header:
         if 'JD' in ff[0].header:
@@ -78,6 +146,65 @@ def sedm_fix_header(fname):
     # Dome status
     if 'DOMEST' not in ff[0].header:
         ff[0].header['DOMEST'] = 'Open'
+    # FOCUS keywords
+    if 'FOCPOS' not in ff[0].header:
+        if 'SECFOCUS' in ff[0].header:
+            focus = ff[0].header['SECFOCUS']
+            ff[0].header['FOCPOS'] = (focus, "Position of secondary focus")
+    # Refraction keywords
+    if 'HA_REFR' not in ff[0].header:
+        if 'HAREFR' in ff[0].header:
+            refr = ff[0].header['HAREFR']
+        elif 'RA_REFR' in ff[0].header:
+            refr = ff[0].header['RA_REFR']
+        else:
+            refr = -999.
+        ff[0].header['HA_REFR'] = (refr, "Telescope HA Refraction")
+
+    # Now verify header
+    for k, v in header_types.items():
+        if v == str:
+            if k not in ff[0].header:
+                ff[0].header[k] = ''
+            else:
+                if type(ff[0].header[k]) != v:
+                    try:
+                        newval = str(ff[0].header[k])
+                        ff[0].header[k] = newval
+                    except ValueError:
+                        ff[0].header[k] = ''
+        elif v == float:
+            if k not in ff[0].header:
+                ff[0].header[k] = -999.
+            else:
+                if type(ff[0].header[k]) != v:
+                    try:
+                        newval = float(ff[0].header[k])
+                        ff[0].header[k] = newval
+                    except ValueError:
+                        ff[0].header[k] = -999.
+        elif v == int:
+            if k not in ff[0].header:
+                ff[0].header[k] = -999
+            else:
+                if type(ff[0].header[k]) != v:
+                    try:
+                        newval = int(ff[0].header[k])
+                        ff[0].header[k] = newval
+                    except ValueError:
+                        ff[0].header[k] = -999
+        elif v == bool:
+            if k not in ff[0].header:
+                ff[0].header[k] = False
+            else:
+                if type(ff[0].header[k]) != v:
+                    try:
+                        newval = bool(ff[0].header[k])
+                        ff[0].header[k] = newval
+                    except ValueError:
+                        ff[0].header[k] = False
+        else:
+            logging.warning("Illegal type for keyword %s" % k)
     # Close
     ff.close()
 
@@ -90,25 +217,20 @@ if __name__ == "__main__":
 
     # setup arguments parser
     parser = argparse.ArgumentParser(
-        description="""Do an aperture extraction of a standard star.""",
+        description="""Fix FITS headers for SEDM observations.""",
         formatter_class=argparse.RawTextHelpFormatter)
     # setup arguments
-    parser.add_argument('indate', type=str, default=None,
-                        help='UT date as YYYYMMDD')
     parser.add_argument('--fspec', type=str, default=None,
-                        help='unique observation identifier')
+                        help='file spec for fits files')
     args = parser.parse_args()
 
-    dd = args.indate
     fspec = args.fspec
 
     if not fspec:
-        logging.info("Usage - hdrfix YYYYMMDD --fspec <filespec>")
+        logging.info("Usage - hdrfix --fspec <filespec>")
     else:
-        # Check inputs and environment
-        reddir = os.environ['SEDMREDUXPATH']
         # get a list of files
-        flist = glob.glob(os.path.join(reddir, dd, fspec))
+        flist = glob.glob(fspec)
         # loop over files
         for file_name in flist:
             logging.info(file_name)
