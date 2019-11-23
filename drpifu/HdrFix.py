4a5f2e827f92cc72512b7090b51b9db1b61e1502
Don Neill <neilljd@gmail.com>
2019-11-23 14:36:47 -0800

    fix to NAME keyword

--- drpifu/HdrFix.py
+++ drpifu/HdrFix.py
@@ -77,7 +77,7 @@ def sedm_fix_header(fname):
                  'CD_LAMP': 'off', 'XE_LAMP': 'off'}
     # image type
     if 'OBJECT' in ff[0].header:
-        obj = ff[0].header['OBJECT'].split('[')[0].strip().replace(" ", "-")
+        obj = ff[0].header['OBJECT'].split('[')[0].strip()
         if 'STD-' in obj:
             ff[0].header['IMGTYPE'] = 'Standard'
             ff[0].header['ABPAIR'] = False
