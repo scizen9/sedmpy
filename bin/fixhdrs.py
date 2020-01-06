0b1e30cf0c3cb1301b50bbe415ff0dcd61842d32
Don Neill <neilljd@gmail.com>
2020-01-06 08:25:05 -0800

    Calling script for HdrFix.py that handles un/gzipping of image files

---
+++ bin/fixhdrs.py
@@ -0,0 +1,48 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import glob
+import subprocess
+import argparse
+rdir = '/scr2/sedm/raw/'
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="update old headers",
+            formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('--start_date', type=str, default='20160601',
+            help='starting date for fixing headers')
+    parser.add_argument('--end_date', type=str, default='20161231',
+            help='ending date for fixing headers')
+    parser.add_argument('--no_rezip', action="store_true", default=False,
+            help='Do not re-gzip image files')
+
+    args = parser.parse_args()
+
+    if not args.start_date or not args.end_date:
+        print("must supply both --start_date and --end_date")
+    else:
+        sdate = int(args.start_date)
+        edate = int(args.end_date)
+        print("start: %d, end: %d" % (sdate, edate))
+        dir_list = glob.glob(os.path.join(rdir, '20??????'))
+        for rd in dir_list:
+            fdate = int(rd.split('/')[-1])
+            if sdate <= fdate <= edate:
+                print(rd)
+                os.chdir(rd)
+                uzlist = os.listdir(rd)
+                print("gunzipping %d files" % len(uzlist))
+                for fl in uzlist:
+                    if '.fits.gz' in fl:
+                        subprocess.run(["gunzip", os.path.join(rd, fl)])
+                subprocess.run(["/scr2/sedmdrp/spy", "/home/sedm/HdrFix.py", "--rawdir", rdir, "--date", rd.split('/')[-1]])
+                if not args.no_rezip:
+                    zlist = os.listdir(rd)
+                    print("gzipping %d files" % len(zlist))
+                    for fl in zlist:
+                        if '.fits' in fl:
+                            subprocess.run(["gzip", os.path.join(rd, fl)])
+                else:
+                    print("skipping gzipping")
+
