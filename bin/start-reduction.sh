c21c68f80274aff3e066e74d252d8c93e39b5a32
Don Neill <neilljd@gmail.com>
2020-02-21 16:32:47 -0800

    realtime reduction script update

--- bin/start-reduction.sh
+++ bin/start-reduction.sh
@@ -3,4 +3,4 @@
 source ~/.bashrc
 
 echo "Starting the daily reduction"
-python /scr2/sedmdrp/sedmpy/drprc/realtimered.py -f -o 1>/tmp/reduction_info 2>/tmp/reduction_error
+python /scr2/sedmdrp/sedmpy/drprc/realtimered.py 1>/tmp/sedmdrp_reduction_info 2>/tmp/sedmdrp_reduction_error
