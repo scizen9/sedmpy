9b51d0a9234d8fe663933e975905b6f0b2ffb719
Don Neill <neilljd@gmail.com>
2020-03-15 16:09:43 -0700

    Handle no files in (empty) directory

--- drpifu/What.py
+++ drpifu/What.py
@@ -3,22 +3,25 @@ import astropy.io.fits as pf
 
 def go(imfiles):
     for ifile in imfiles:
-        FF = pf.open(ifile)
-        header = FF[0].header
-        header['FNAME'] = ifile.split('.gz')[0]
-        if 'AIRMASS' not in header:
-            header['AIRMASS'] = -1.
-        if 'ADCSPEED' not in header:
-            header['ADCSPEED'] = -1.
-        if 'EXPTIME' not in header:
-            header['EXPTIME'] = -1.
-        if 'OBJECT' not in header:
-            header['OBJECT'] = 'ERR!'
         try:
-            print("%(FNAME)28s (%(AIRMASS)1.3f/%(ADCSPEED)1.1f/%(EXPTIME)s s):"
-                  " %(OBJECT)-s" % header)
-        except:
-            print("%28s : ?" % ifile.split('.gz')[0])
+            FF = pf.open(ifile)
+            header = FF[0].header
+            header['FNAME'] = ifile.split('.gz')[0]
+            if 'AIRMASS' not in header:
+                header['AIRMASS'] = -1.
+            if 'ADCSPEED' not in header:
+                header['ADCSPEED'] = -1.
+            if 'EXPTIME' not in header:
+                header['EXPTIME'] = -1.
+            if 'OBJECT' not in header:
+                header['OBJECT'] = 'ERR!'
+            try:
+                print("%(FNAME)28s (%(AIRMASS)1.3f/%(ADCSPEED)1.1f/%(EXPTIME)s"
+                      " s): %(OBJECT)-s" % header)
+            except:
+                print("%28s : ?" % ifile.split('.gz')[0])
+        except FileNotFoundError:
+            pass
 
 
 if __name__ == '__main__':
