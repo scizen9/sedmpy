9b397e9ab4c46d86377ba6d69e3391989d19ff90
Don Neill <neilljd@gmail.com>
2020-05-05 20:14:49 -0700

    Tab character error in Plan.py

--- drpifu/Plan.py
+++ drpifu/Plan.py
@@ -243,7 +243,7 @@ classify:
 	$(REPORT) | tee report.txt
 
 finalreport:
-    $(REPORT) comment | tee report.txt
+	$(REPORT) comment | tee report.txt
 	cat report*.txt | mail -s "SEDM DRP Report for $(current_dir)" neill@srl.caltech.edu,rsw@astro.caltech.edu,fremling@caltech.edu,jesper@astro.su.se,tda@lists.astro.caltech.edu
 
 """
