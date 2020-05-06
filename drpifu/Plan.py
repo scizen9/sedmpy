eec46e6add6498540f66ca0ebdf68bae7e53d39e
Don Neill <neilljd@gmail.com>
2020-05-06 07:36:37 -0700

    clean up report.txt for finalreport target in Plan.py

--- drpifu/Plan.py
+++ drpifu/Plan.py
@@ -244,7 +244,7 @@ classify:
 
 finalreport:
 	$(REPORT) comment | tee report.txt
-	cat report*.txt | mail -s "SEDM DRP Report for $(current_dir)" neill@srl.caltech.edu,rsw@astro.caltech.edu,fremling@caltech.edu,jesper@astro.su.se,tda@lists.astro.caltech.edu
+	cat report*.txt | tail -n +2 | mail -s "SEDM DRP Report for $(current_dir)" neill@srl.caltech.edu,rsw@astro.caltech.edu,fremling@caltech.edu,jesper@astro.su.se,tda@lists.astro.caltech.edu
 
 """
 
