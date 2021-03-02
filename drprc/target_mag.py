16f39ed93ead803e925f549d67dd7be2ed266068
scizen9 <neilljd@gmail.com>
2021-03-02 11:03:25 -0800

    Added error calculation and centroiding of standard stars, converted to electrons, accounted for exposure time and increased default aperture sizes.

--- drprc/target_mag.py
+++ drprc/target_mag.py
@@ -7,7 +7,7 @@ Created on Wed Feb 24 09:36:52 2021
 import math
 from astropy.io import fits as pf
 from astropy.stats import sigma_clipped_stats
-from photutils import aperture_photometry, CircularAperture, CircularAnnulus
+from photutils import aperture_photometry, CircularAperture, CircularAnnulus, centroid_sources, centroid_2dg
 import numpy as np
 
 sdss_r_band_extinction_coeff = 0.12
@@ -27,9 +27,17 @@ stds = {
     }    # r = V - 0.46 * (B-V) + 0.11
 
 
-def get_target_mag(imfile, aper_r=4.0, sky_aper_in=10., sky_aper_out=15.,
+def get_target_mag(imfile, aper_r=40., sky_aper_in=50., sky_aper_out=65.,
                    zeropoint=None, verbose=False):
-    """get target mag"""
+    """get target mag
+    Inputs
+    imfile: (str) - filename
+    aper_r: (float) - aperture radius in pixels
+    sky_aper_in: (float) - sky annulus inner radius
+    sky_aper_out: (float) - sky annulus outer radius
+    zeropoint: (float) - magnitude zeropoint
+    verbose: (bool) - extra output?
+    """
     # initialize outputs
     targ_mag = None
     targ_magerr = None
@@ -49,8 +57,28 @@ def get_target_mag(imfile, aper_r=4.0, sky_aper_in=10., sky_aper_out=15.,
         targ_y = hdu[0].header['TARGYPX']
         targ_air = hdu[0].header['AIRMASS']
         targ_name = hdu[0].header['OBJECT']
+        targ_expt = hdu[0].header['EXPTIME']
+        targ_gain = hdu[0].header['GAIN']
+        targ_rnoise = hdu[0].header['RDNOISE']
+
         if verbose:
-            print("x: %.2f, y: %.2f, air: %.3f, targ: %s" % (targ_x, targ_y, targ_air, targ_name))
+            print("%s | x: %.2f, y: %.2f, expt: %.2f, air: %.3f, gain: %.3f, rn: %.1f" %
+                  (targ_name, targ_x, targ_y, targ_expt, targ_air, targ_gain, targ_rnoise))
+
+        # Centroid STD stars
+        if 'STD' in targ_name:
+            cent_x, cent_y = centroid_sources(image, targ_x, targ_y, box_size=int(aper_r),
+                                              centroid_func=centroid_2dg)
+            targ_x = cent_x[0]
+            targ_y = cent_y[0]
+            if verbose:
+                print("Position updated | x: %.2f, y: %.2f" % (targ_x, targ_y))
+
+        # Get error image
+        erimg = np.sqrt(image * targ_gain + targ_rnoise ** 2)
+
+        # Convert DN to electrons
+        image *= targ_gain
 
         # Are we a standard star?
         if 'STD' in targ_name:
@@ -75,22 +103,31 @@ def get_target_mag(imfile, aper_r=4.0, sky_aper_in=10., sky_aper_out=15.,
             bkg_median.append(median_sigclip)
         bkg_median = np.array(bkg_median)
 
-        # Calculate net aperture sum
-        phot = aperture_photometry(image, apertures)
+        # Calculate net aperture sums
+        phot = aperture_photometry(image, apertures, error=erimg)
         phot['annulus_median'] = bkg_median
         phot['aper_bkg'] = bkg_median * apertures.area
+
+        # Subtract background
         phot['aper_sum_bkgsub'] = phot['aperture_sum'] - phot['aper_bkg']
+
+        # Extract data
         ap_sum_bkgsub = phot['aper_sum_bkgsub'].data[0]
+        ap_sum_err = phot['aperture_sum_err'].data[0]
+
+        # Get e-/s
+        ap_sum_bkgsub_per_sec = ap_sum_bkgsub / targ_expt
 
         # Is the net sum positive?
-        if ap_sum_bkgsub > 0:
+        if ap_sum_bkgsub_per_sec > 0:
 
             # Calculate airmass-corrected instrumental magnitude
-            int_mag = math.log10(ap_sum_bkgsub) - targ_air * sdss_r_band_extinction_coeff
+            air_cor = targ_air * sdss_r_band_extinction_coeff
+            int_mag = -2.5 * math.log10(ap_sum_bkgsub_per_sec) - air_cor
 
             # Generate a new zeropoint, if we are a standard star
             if std_mag is not None:
-                std_zeropoint = std_mag + int_mag
+                std_zeropoint = std_mag - int_mag
                 zp = std_zeropoint
                 if verbose:
                     print("std_mag: %2f, std_zp: %.2f" % (std_mag, zp))
@@ -102,12 +139,15 @@ def get_target_mag(imfile, aper_r=4.0, sky_aper_in=10., sky_aper_out=15.,
                 zp = zeropoint
 
             # Calculated calibrated target magnitude
-            targ_mag = zp - int_mag
+            targ_mag = int_mag + zp
+
+            # Calculated magnitude error
+            targ_magerr = 1.0857362 * ap_sum_err / ap_sum_bkgsub
 
             if verbose:
-                print("cnts: %.1f, imag: %.2f, tmag: %.2f, zp: %.2f" % (ap_sum_bkgsub, int_mag, targ_mag, zp))
+                print("e-/s: %.1f, err: %.1f, excor: %.2f, imag: %.2f, zp: %.2f, targ_mag: %.2f +- %.2f" %
+                      (ap_sum_bkgsub_per_sec, ap_sum_err, air_cor, int_mag, zp, targ_mag, targ_magerr))
 
-            targ_magerr = targ_mag / 100.
         else:
             print("Warning: negative net aperture sum, no mag calculated!")
 
