919a091511fdd8d74e3c3d703108290ccf4c64fa
scizen9 <neilljd@gmail.com>
2021-03-03 08:45:00 -0800

    Initial Revision of script to do photometry

---
+++ bin/do_phot.py
@@ -0,0 +1,22 @@
+import glob
+try:
+    from target_mag import get_target_mag
+except ImportError:
+    from drprc.target_mag import get_target_mag
+
+try:
+    import fitsutils
+except ImportError:
+    import drprc.fitsutils as fitsutils
+
+phot_zp = None
+flist = glob.glob('rc*.fits')
+for rf in flist:
+    if fitsutils.get_par(rf, "ONTARGET"):
+        print("\nGetting target mag for %f" % rf)
+        target_mag, target_magerr, std_zp = get_target_mag(rf, zeropoint=phot_zp)
+        print("Quick MAG = %.3f +- %.3f" % (target_mag, target_magerr))
+        if std_zp is not None:
+            print("Quick MAG_ZP: %.3f" % std_zp)
+            if phot_zp is None:
+                phot_zp = std_zp
