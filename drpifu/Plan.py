d7906cf842aa8c94d8fe5ddc488c9979e3402908
Don Neill <neilljd@gmail.com>
2021-06-09 15:03:48 -0700

    changed redux clean target to post_redux in Plan.py

--- drpifu/Plan.py
+++ drpifu/Plan.py
@@ -251,8 +251,8 @@ finalreport:
 	$(REPORT) comment | tee report.txt
 	cat report*.txt | tail -n +2 | mail -s "SEDM DRP Report for $(current_dir)" neill@srl.caltech.edu,rsw@astro.caltech.edu,fremling@caltech.edu,jesper@astro.su.se,m.rigault@ipnl.in2p3.fr,lezmy@ipnl.in2p3.fr,tda@lists.astro.caltech.edu
 
-clean:
-    $(PY) $(PYC)/AutoReduce.py --clean --date $(current_dir)
+post_redux:
+	$(PY) $(PYC)/AutoReduce.py --clean --date $(current_dir)
 
 """
 
