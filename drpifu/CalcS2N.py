d0f14eca909045df1866437a4e435c8e01fd77b2
Don Neill <neilljd@gmail.com>
2021-06-18 17:05:00 -0700

    handle nan value in CalcS2N.py

--- drpifu/CalcS2N.py
+++ drpifu/CalcS2N.py
@@ -62,6 +62,8 @@ def calc_s2n(spec_file=None, start_wave=4000., end_wave=8000., overwrite=False):
             sw1 = end_wave
         sample_range = [i for i, w in enumerate(wave) if sw0 < w < sw1]
         s2nmed = np.nanmedian(s2nspec[sample_range])
+        if s2nmed != s2nmed:
+            s2nmed = 1.0
 
         # Update ascii file
         with open(spec_file, "r") as specIn:
