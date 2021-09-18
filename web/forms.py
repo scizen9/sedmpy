f017350c3d041725c3ca16ec033297199bde04a4
Don Neill <neilljd@gmail.com>
2021-09-18 07:45:28 -0700

    Update request form labels in forms.py

--- web/forms.py
+++ web/forms.py
@@ -41,9 +41,9 @@ class AddFixedRequest(FlaskForm):
     marshal_id = fields.HiddenField('Hidden', default=-1)
     user_id = fields.HiddenField('Hidden')
     allocation_id = fields.HiddenField('Hidden')
-    obj_name = fields.StringField('Object Name (Names should be limited to 26 '
-                                  'Characters and have no blank spaces are '
-                                  'special characters such as ["*_.] '
+    obj_name = fields.StringField('Object name (Names should be limited to 26 '
+                                  'Characters and have no blank spaces or '
+                                  'special characters such as ["*_.], '
                                   'dashes are acceptable)',
                                   [validators.input_required()])
     status = fields.SelectField('Status',
@@ -58,10 +58,10 @@ class AddFixedRequest(FlaskForm):
     obj_dec = fields.StringField('DEC (DD:MM:SS or Degrees)')
     obj_epoch = fields.StringField('EPOCH', default='2000')
     obj_mag = fields.FloatField('Magnitude')
-    allocation = fields.SelectField('allocation')
+    allocation = fields.SelectField('Allocation')
 
-    priority = fields.FloatField('priority', default=.99)
-    filters_op = fields.SelectField('filters',
+    priority = fields.FloatField('Priority', default=.99)
+    filters_op = fields.SelectField('Filters',
                                     coerce=str,
                                     choices=[(' 1, 1, 1, 1}', 'u-g-r-i'),
                                              (' 0, 1, 1, 1}', 'g-r-i'),
@@ -84,53 +84,53 @@ class AddFixedRequest(FlaskForm):
 
     rc_use_mag = fields.BooleanField(
         'Use object magnitude to set exposure time')
-    seq_repeats = fields.IntegerField('# of Repeated Sequences', default=1)
-    seq_completed = fields.IntegerField('# of Sequences Already Completed',
+    seq_repeats = fields.IntegerField('# of repeated sequences', default=1)
+    seq_completed = fields.IntegerField('# of sequences already completed',
                                         default=0)
-    ifu = fields.BooleanField('Take IFU Exposure')
-    rc = fields.BooleanField('Take RC Exposure')
+    ifu = fields.BooleanField('Take IFU exposure')
+    rc = fields.BooleanField('Take RC exposure')
     ifu_use_mag = fields.BooleanField(
         'Use object magnitude to set exposure time')
     ifu_exptime = fields.IntegerField(
-        'Enter Total IFU Exposure Time in seconds')
+        'Enter total IFU exposure time in seconds')
     ab = fields.BooleanField('Select For AB pair')
 
-    cadence = fields.FloatField('cadence', default=None)
+    cadence = fields.FloatField('Cadence', default=None)
 
     min_moon_dist = fields.FloatField(
-        'minimum moon distance (degrees)',
+        'Min moon dist. (deg)',
         validators=(validators.Optional(),
                     validators.number_range(0., 180.)), default=30)
 
     max_moon_illum = fields.FloatField(
-        'Maximum Moon Illumination (fractional 0 to 1)',
+        'Max moon phase (0. to 1.)',
         validators=(validators.Optional(),
                     validators.number_range(0., 1.)), default=1)
     maxairmass = fields.FloatField(
-        'Maximum Airmass',
+        'Max airmass',
         validators=(validators.Optional(),
                     validators.number_range(1, 5)), default=2.5)
     max_cloud_cover = fields.FloatField(
-        'Maximum Cloud Cover (fractional)',
+        'Max cloud cover (0. to 1.)',
         validators=(validators.Optional(),
                     validators.number_range(0., 1.)), default=1)
     max_fwhm = fields.FloatField(
-        'Maximum FWHM',
+        'Max FWHM (asec)',
         validators=(validators.Optional(),
                     validators.number_range(0., 10.)), default=10)
-    phasesamples = fields.FloatField('samples per period', default=None)
-    sampletolerance = fields.FloatField('samples tolerance', default=None)
-    inidate = fields.DateField('start date (Y-m-d)',
+    phasesamples = fields.FloatField('Samples per period', default=None)
+    sampletolerance = fields.FloatField('Samples tolerance', default=None)
+    inidate = fields.DateField('Start date (Y-m-d)',
                                validators=[validators.input_required()],
                                format='%Y-%m-%d',
                                default=datetime.datetime.utcnow())
 
-    enddate = fields.DateField('end date (Y-m-d)',
+    enddate = fields.DateField('End date (Y-m-d)',
                                validators=[validators.input_required()],
                                format='%Y-%m-%d',
                                default=(datetime.datetime.utcnow() +
                                         datetime.timedelta(days=3)))
-    creationdate = fields.Label('creationdate', 'Creation Date')
-    lastmodified = fields.Label('lastmodified', 'Last Modified')
+    creationdate = fields.Label('creationdate', 'Creation date')
+    lastmodified = fields.Label('lastmodified', 'Last modified')
     last_obs_jd = fields.Label('last_obs_jd', 'Last observation')
-    submit_req = fields.SubmitField('Submit Request')
+    submit_req = fields.SubmitField('Submit request')
