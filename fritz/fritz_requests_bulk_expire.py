26c7c5d03d9c61611921385909af9aac07949189
Don Neill <neilljd@gmail.com>
2021-09-23 14:48:48 -0700

    Initial Revision of fritz_requests_bulk_expire.py

---
+++ fritz/fritz_requests_bulk_expire.py
@@ -0,0 +1,56 @@
+import requests
+import argparse
+
+from marshals.interface import api, update_status_request
+import db.SedmDb
+
+import datetime
+
+# URL constants
+fritz_base_url = 'https://fritz.science/'
+fritz_view_source_url = fritz_base_url + 'source'
+fritz_req_url = fritz_base_url + 'api/followup_request/'
+
+# Use the database
+sedmdb = db.SedmDb.SedmDB()
+
+
+def get_all_fritz_reqests():
+    """Query fritz for followup requests"""
+    try:
+        res = api('GET', fritz_req_url).json()
+    except requests.exceptions.ConnectionError:
+        res = {'status': 'Error', 'message': 'ConnectionError', 'data': None}
+
+    if 'success' in res['status']:
+        data = res['data']
+    else:
+        data = None
+
+    return data
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="""
+                         
+Expires outdated requests on the fritz marshal.
+                         
+""",
+        formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('--testing', action="store_true", default=False,
+                        help='Do not actually post to marshal (for testing)')
+    args = parser.parse_args()
+
+    reqs = get_all_fritz_reqests()
+
+    for r in reqs:
+        if 'submitted' in r['status']:
+            pay = r['payload']
+            end_date = pay['end_date']
+            if datetime.date(*[int(du) for du in
+                               end_date.split('-')]) < datetime.date.today():
+                marshal_req_id = r['id']
+                res = update_status_request('Expired', marshal_req_id, 'fritz',
+                                            testing=True)
+                break
