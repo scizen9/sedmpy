dbc8c44f06af44c406c76c375544dc40b0888705
Don Neill <neilljd@gmail.com>
2022-01-27 17:22:19 -0800

    Yet another try at forcing what.list to display as text in model.py

--- web/model.py
+++ web/model.py
@@ -1421,9 +1421,7 @@ def get_ifu_products(obsdir, user_id, obsdate="", show_finder=True,
         else:
             grw_report = ""
         if os.path.exists(os.path.join(obsdir, 'what.list')):
-            os.symlink(os.path.join(obsdir, 'what.list'),
-                       os.path.join(obsdir, 'what.txt'))
-            wha_report = """<a href="http://pharos.caltech.edu/data_r/redux/{0}/what.txt" type="plain/text">What</a>""".format(obsdate)
+            wha_report = """<a href="http://pharos.caltech.edu/data_r/redux/{0}/what.list" type="plain/text">What</a>""".format(obsdate)
         else:
             wha_report = ""
         div_str += """<div class="row">"""
