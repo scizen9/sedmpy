16ada49ed2d370ec7c515a664df0d4c959e9d352
Don Neill <neilljd@gmail.com>
2022-11-08 17:16:49 -0800

    Do overscan subtraction in Debias.py

--- drpifu/Debias.py
+++ drpifu/Debias.py
@@ -15,10 +15,10 @@ Note:
 
 """
 import time
-import numpy as np
 import astropy.io.fits as pf
-from scipy.ndimage import filters
+import numpy as np
 import sedmpy_version
+from Imcombine import subtract_oscan
 
 drp_ver = sedmpy_version.__version__
 
@@ -40,54 +40,6 @@ def add_prefix(fname):
     return "/".join(sp)
 
 
-def calculate_overscan(dat):
-    """Calculate smoothed overscan vector
-
-    Args:
-        dat (numpy array): image frame
-
-    Returns:
-        numpy vector: median smoothed overscan vector
-
-    """
-
-    oscan = np.nanmedian(dat[:, 2045:], axis=1)
-    oscan = oscan.astype(np.float32)
-    smooth = filters.median_filter(oscan, size=50)
-
-    return np.tile(smooth, (2048, 1))
-
-
-def remove(fits_obj):
-    """Return the overscan-subtracted and gain-corrected version of the fits object
-
-    Args:
-        fits_obj (fits object): fits science image to be overscan-subtracted
-
-    Returns:
-        data array: bias-subtracted and gain corrected image
-
-    """
-
-    dat = fits_obj[0].data
-
-    # Gain for electron conversion
-    try:
-        gain = fits_obj[0].header['GAIN']
-    except:
-        gain = 1.8  # Guess the gain
-
-    # get overscan if correctly sized
-    print(dat.shape)
-    if dat.shape == (2048, 2048):
-        oscan_img = calculate_overscan(dat)
-        ret = (dat - oscan_img.T) * gain
-    else:
-        ret = dat * gain
-    # return overscan subtracted image
-    return ret
-
-
 if __name__ == '__main__':
     import sys
 
@@ -104,27 +56,38 @@ if __name__ == '__main__':
         adcspeed = FF[0].header['ADCSPEED']
 
         bfname = "bias%1.1f.fits" % adcspeed
-        mbias = pf.open(bfname)
+        try:
+            mbias = pf.open(bfname)
+        except FileNotFoundError:
+            print("Master bias not found: %s" % bfname)
+            continue
 
-        # Bias frame subtraction
-        FF[0].data = FF[0].data - mbias[0].data
+        # Subtract overscan
+        img = FF[0].data
+        img = img.astype(np.float32)
+        img -= subtract_oscan(img, FF[0].header)
 
-        # Overscan subtraction
-        FF[0].data = remove(FF)
+        # Gain value
+        try:
+            gain = FF[0].header['GAIN']
+        except KeyError:
+            gain = 1.8
+
+        # Bias frame subtraction and gain correction
+        FF[0].data = (img - mbias[0].data) * gain
 
         outname = add_prefix(ifile)
-        FF[0].header['BIASSUB'] = ('Subtracted',
-                                   'Ovrscn + bias handled by Debias.py')
-        FF[0].header['BIASSUB2'] = (bfname, 'Bias file used')
+        FF[0].header['BIASSUB'] = (True, 'Bias subtracted?')
+        FF[0].header['MBIASFN'] = (bfname, 'Master Bias file used')
         try: 
             GAIN = FF[0].header['GAIN']
             FF[0].header['GAIN'] = (1.0, 'GAIN Adjusted (was %s)' % GAIN)
-        except: 
+        except KeyError:
             GAIN = 1.8  # Guess the gain
             FF[0].header['GAIN'] = (1.0,
                                     'GAIN Adjusted (was guessed %s)' % GAIN)
         FF[0].header['BUNIT'] = 'electron'
-        FF[0].header.add_history('drpifu.Debias run on %s' % time.strftime("%c"))
+        FF[0].header.add_history('drpifu.Debias run on %s' %
+                                 time.strftime("%c"))
         FF[0].header['DRPVER'] = drp_ver
         FF.writeto(outname)
-
